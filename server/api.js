ce80d402ac52b0e4d76dd0bcf803c82bc9094bd3
Kevin <kevin.alexanderra1302@gmail.com>
2023-09-16 07:19:47 -0400

    we need fix export modules

--- server/api.js
+++ server/api.js
@@ -1,23 +1,23 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+const app = express()
+import port from '../port.js'
+const { PORT } = port
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
 
-const PORT = process.env.PORT ?? 3000;
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-
-const data = require('./products.json');
+const data = require('./products.json')
 
 app.get('/', (req, res) => {
-  res.json(data);
-});
+  res.json(data)
+})
 
 app.get('/Products/:id', (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
 
-  const product = data[0].productos.find((obj) => obj.id === id);
-  res.json(product);
-});
+  const product = data[0].productos.find((obj) => obj.id === id)
+  res.json(product)
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
