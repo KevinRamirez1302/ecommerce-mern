fabafc99691962ef1dcf3fc12d73008d18d9a56d
kevin <kevin.alexanderra1302@gmail.com>
2023-09-27 05:24:05 -0400

    add controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,18 @@
-import { userModel } from '../models/user.model.js'
-export const register = (req, res) => {
-  res.send('hola')
-  console.log(req.body)
-}
+import { userModel } from '../models/user.model.js';
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-export const login = (req, res) => res.send('Login')
+    const newUser = new userModel({
+      name,
+      email,
+      password,
+    });
+
+    res.json(newUser);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+export const login = (req, res) => res.send('Login');
