23e7e7066c5ec129bdf1ca3d0e6ab7e753267585
kevin <kevin.alexanderra1302@gmail.com>
2023-10-14 19:08:49 -0400

    tailwind navbar,register and login

--- Client/src/Views/login/login.jsx
+++ Client/src/Views/login/login.jsx
@@ -1,9 +1,24 @@
 export const Login = () => {
   return (
     <>
-      <form action="">
-        <input type="text" name="email" placeholder="Email" />
-        <input type="text" name="Password" placeholder="Password" />
+      <form
+        className="flex flex-col items-center justify-center h-80"
+        action=""
+      >
+        <h2 className="text-center m-4 ">Login</h2>
+        <input
+          className="w-60 bg-gray-200 transition-all m-1 text-black p-2 px-4 rounded"
+          type="email"
+          placeholder="email"
+        />
+        <input
+          className=" w-60 bg-gray-200 transition-all m-1 text-black p-2 px-4 rounded"
+          type="text"
+          placeholder="Password"
+        />
+        <button className="bg-violet-800 pl-5 pr-5 pt-2 pb-2  w-40 m-4">
+          Enter
+        </button>
       </form>
     </>
   );
--- Client/src/Views/register/register.jsx
+++ Client/src/Views/register/register.jsx
@@ -1,11 +1,36 @@
 export const Register = () => {
   return (
     <>
-      <form className="flex-row" action="">
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="email" />
-        <input type="text" placeholder="Password" />
-      </form>
+      <section className="w-full h-full flex items-center justify-center">
+        <form
+          className=" w-1/2 flex flex-col items-center justify-center h-80"
+          action=""
+        >
+          <h2 className="text-center m-4 ">Register</h2>
+          <input
+            className="w-60 bg-gray-200 transition-all m-1 text-black p-2 px-4 rounded"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="w-60 bg-gray-200 transition-all m-1 text-black p-2 px-4 rounded"
+            type="email"
+            placeholder="email"
+          />
+          <input
+            className=" w-60 bg-gray-200 transition-all m-1 text-black p-2 px-4 rounded"
+            type="text"
+            placeholder="Password"
+          />
+          <button className=" bg-violet-800 pl-5 pr-5 pt-2 pb-2  w-40 m-4">
+            Enter
+          </button>
+        </form>
+        <div className=" w-1/2 flex flex-col items-center justify-center h-screen bg-violet-900">
+          <h1>New Here</h1>
+          <p>Sign Up and discover all things</p>
+        </div>
+      </section>
     </>
   );
 };
