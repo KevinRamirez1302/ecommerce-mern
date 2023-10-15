4b78dd06bb8b23ef80c097d69d6d1fd103a1b738
Kevin <kevin.alexanderra1302@gmail.com>
2023-10-15 07:03:32 -0400

    asd

--- Client/src/Views/register/register.jsx
+++ Client/src/Views/register/register.jsx
@@ -1,11 +1,11 @@
 export const Register = () => {
   return (
     <>
-      <form className="flex-row" action="">
+      <form className="" action="">
         <input type="text" placeholder="Name" />
         <input type="email" placeholder="email" />
         <input type="text" placeholder="Password" />
       </form>
     </>
-  );
-};
+  )
+}
